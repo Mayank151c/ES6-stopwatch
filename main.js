ab68c5e2db3c17f0c26a65fb3a708011cf26dc25
Mayank Choudhary <66920751+Mayank151c@users.noreply.github.com>
2021-06-05 23:59:46 +0530

    Adding required functions

---
+++ main.js
@@ -0,0 +1,55 @@
+//variables to store time in integer
+var hours=0,minutes=0,seconds=0,miliseconds=0;
+
+//variables to store time in string
+var HH,MM,SS,mm;
+
+//
+var timer = document.getElementsByClassName("timer")[0];
+var interval;
+timer.innerHTML = "00 : 00 : 00 : 00";
+
+
+function start() {
+
+	interval = setInterval(function() {
+		miliseconds++;
+
+		if(miliseconds>99) {
+			seconds++;	miliseconds=0;
+		}
+
+		if(seconds>59) {
+			minutes++;	seconds=0;
+		}
+
+		if(minutes>59) {
+			hours++;	minutes=0;
+		}
+
+		if(seconds 	   < 10)	SS = "0" + seconds; 	else SS = seconds;
+
+		if(minutes	   < 10)	MM = "0" + minutes;	 	else MM = minutes;
+		
+		if(hours	   < 10)	HH = "0" + hours;		else HH = hours;
+		
+		if(miliseconds < 10)	mm = "0" + miliseconds; else mm = miliseconds;
+
+		//updating timer every (1/100)th second
+		timer.innerHTML = HH + " : " + MM + " : " + SS + " : " + mm;
+
+	},10)
+}
+
+
+function stop() {
+	flag=0;
+	clearInterval(interval);
+}
+
+
+function reset() {
+	stop();
+	hours=minutes=seconds=miliseconds=0;
+	timer.innerHTML = "00 : 00 : 00 : 00";
+}
